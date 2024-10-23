e1ebb5a5dcc946e924f3991c1956c5542b1209fe
Ishan-Khurram <mo.ishan.khurram@gmail.com>
2024-10-23 14:02:20 -0400

    Initial Xcode Commit

---
+++ MyotropeTests/MyotropeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyotropeTests.swift
+//  MyotropeTests
+//
+//  Created by Ishan Khurram on 2024-10-23.
+//
+
+import Testing
+@testable import Myotrope
+
+struct MyotropeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
